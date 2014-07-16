f579820625d351972266fa8fd82d9cf19164df0b
jose rangel <jryyf1518@gmail.com>
2014-07-16 11:44:48 -0430

    script

---
+++ script/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/
+fecha=$(date +"%m-%d-%Y-%T")
+
+echo *************************SCRIPT.SH**********************
+
+
+cd /home/academia/
+
+cd respaldo
+
+cd /home/academia/respaldo
+
+cp -R /home/academia/control/ /home/academia/respaldo/respaldo$fecha
+
+ **************** ...... FIND DEL  SCRIPT...  ****************
+
+**************** .......FIN DEL RESPALDO......********************
