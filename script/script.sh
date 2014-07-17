167a6c54ebc9cf30f8ec3db64161502409de2a0b
jose rangel <jryyf1518@gmail.com>
2014-07-17 08:44:00 -0430

    cursosaslapure

--- script/script.sh
+++ script/script.sh
@@ -1,17 +1,8 @@
 #!/bin/bash/
 fecha=$(date +"%m-%d-%Y-%T")
-
 echo *************************SCRIPT.SH**********************
-
-
 cd /home/academia/
-
 cd respaldo
-
-cd /home/academia/respaldo
-
-cp -R /home/academia/control/ /home/academia/respaldo/respaldo$fecha
-
- **************** ...... FIND DEL  SCRIPT...  ****************
-
+cp -R /home/academia/control /home/academia/respaldo/respaldo$fecha
+echo *************************SCRIPT.SH**********************
 **************** .......FIN DEL RESPALDO......********************
